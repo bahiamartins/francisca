1f9c8eead8f66422d62ecd30f9e89b63051f37e4
Ronaldo Bahia <bahiamartins@hotmail.com>
2012-07-08 19:34:18 -0300

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteFrancisca::Application.config.secret_token = '78240bc26cf6861d714c4c6bd94f51760b82f13b91e73820ff864fc1a04663dd4df14faae66454036aaa8d690d5814ffbeafa77cfdadfbef0d64482cce48c55b'
